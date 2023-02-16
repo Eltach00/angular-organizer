bfb605348fc8e93f4954859a1e4525df5f0c10f8
Eltach00 <eltacquliyev@gmail.com>
2023-02-16 12:22:39 +0400

    calendar component *generate function

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -28,6 +28,28 @@ export class CalendarComponent implements OnInit {
   }
 
   generate(now: moment.Moment) {
-    console.log(now.format());
+    const startDay = now.clone().startOf('month').startOf('week');
+    const endDay = now.clone().endOf('month').endOf('week');
+    const date = startDay.clone().subtract(1, 'day');
+    const calendar = [];
+    while (date.isBefore(endDay, 'day')) {
+      calendar.push({
+        days: Array(7)
+          .fill(0)
+          .map(() => {
+            const value = date.add(1, 'day').clone();
+            const active = moment().isSame(value, 'date');
+            const disabled = !now.isSame(value, 'month');
+            const selected = now.isSame(value, 'date');
+            return {
+              value,
+              active,
+              disabled,
+              selected,
+            };
+          }),
+      });
+    }
+    console.log(calendar);
   }
 }
