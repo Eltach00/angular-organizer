05ef3b9a969b9a3450fd4b65dbc795db2c368961
Eltach00 <eltacquliyev@gmail.com>
2023-02-17 11:53:54 +0400

    Organizer template and styles

--- src/app/components/organizer/organizer.component.ts
+++ src/app/components/organizer/organizer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/shared/services/data.service';
 
 @Component({
   selector: 'app-organizer',
   templateUrl: './organizer.component.html',
-  styleUrls: ['./organizer.component.scss']
+  styleUrls: ['./organizer.component.scss'],
 })
 export class OrganizerComponent {
-
+  constructor(public dateServ: DataService) {}
 }
